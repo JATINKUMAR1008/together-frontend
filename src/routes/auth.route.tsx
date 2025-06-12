a050492f9e749dd7b7cb6640b398dcd3bc990970
Jatin Kumar <jatinkumar10082003@gmail.com>
2025-06-12 21:28:33 +0530

    intorduced proxy with vite

--- src/routes/auth.route.tsx
+++ src/routes/auth.route.tsx
@@ -7,7 +7,7 @@ const AuthRoute = () => {
   const location = useLocation();
   const { data: authData, isLoading } = useAuth();
   const user = authData?.user;
-
+ 
   const _isAuthRoute = isAuthRoute(location.pathname);
 
   if (isLoading && !_isAuthRoute) return <DashboardSkeleton />;
